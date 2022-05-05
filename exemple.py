8e797fac3c19060b40a6626140512fa3900f8063
pf4-DEV <74263128+elydre@users.noreply.github.com>
2022-05-05 10:02:08 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,9 +1,10 @@
-# on import le module costom
+# on import le module POOcom
 from POOcom import ClientCom
 
 client = ClientCom()
 
-# on définit une fonction qui sera appelée lorsqu'on reçoit un message
+# on définit une fonction qui sera
+# appelée lorsqu'on reçoit un message
 @client.on_message
 def new(msg):
     print(f"-> {msg}")
